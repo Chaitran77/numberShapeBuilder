dc46a0968bab692e83bf98b47556b150610542dd
Chaitran77 <kiran@inteleyes.com>
2021-09-20 10:30:21 +0100

    Builds triangle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int maxNum = scanner.nextInt();
+        for (int l = 0; l < 2; l++) {
+            if (l == 0) {
+                for (int i = 1; i <= maxNum; i++) {
+                    for (int j = 1; j <= i; j++) {
+                        System.out.print(j);
+                    }
+                    System.out.println();
+                }
+            } else {
+                for (int r = maxNum; r >= 0; r--) {
+                    for (int t = 1; t <= r; t++) {
+                        System.out.print(t);
+                    }
+                    System.out.println();
+                }
+            }
+        }
+
     }
 }
