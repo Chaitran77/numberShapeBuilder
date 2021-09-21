87adfc8438d4ec801ec8f142fc84c6cea5ce83e6
Kiran <kiran@inteleyes.com>
2021-09-21 09:50:22 +0100

    Generates a spaced out number triangle. Cleaned up code a bit too.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,19 +8,24 @@ public class Main {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Type the maximum number: \n");
+
         int maxNum = scanner.nextInt();
-        for (int l = 0; l < 2; l++) {
-            if (l == 0) {
-                for (int i = 1; i <= maxNum; i++) {
-                    for (int j = 1; j <= i; j++) {
-                        System.out.print(j);
+        int loop_iter;
+        int loop_num;
+
+        for (int loopDir = 0; loopDir < 2; loopDir++) {
+            if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
+                for (loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
+                    for (loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
+                        System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
                     }
                     System.out.println();
                 }
             } else {
-                for (int r = maxNum; r >= 0; r--) {
-                    for (int t = 1; t <= r; t++) {
-                        System.out.print(t);
+                for (loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
+                    for (loop_num = 1; loop_num <= loop_iter; loop_num++) {
+                        System.out.print(" " + loop_num);
                     }
                     System.out.println();
                 }
