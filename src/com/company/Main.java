dfb50e3dc93cea85947a95251fce54d6e471e12d
Kiran <kiran@inteleyes.com>
2021-09-21 10:07:32 +0100

    Created mechanism for choosing shapes.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,33 +4,70 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static char getShape(Scanner scanner) {
+        System.out.println("Type the shape to generate (t/triangle, d/diamond): \n");
+        switch (scanner.next().charAt(0)) {
+            case 't':
+                return 't';
+            case 'd':
+                return 'd';
+            default:
+                System.out.println("Invalid input - try again.");
+                getShape(scanner);
+        }
+        return 'A'; // will never reach this
+    }
+
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Type the maximum number: \n");
+        char shape = getShape(scanner);
 
+        System.out.println("Type the maximum number: \n");
         int maxNum = scanner.nextInt();
+
         int loop_iter;
         int loop_num;
 
-        for (int loopDir = 0; loopDir < 2; loopDir++) {
-            if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
-                for (loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
-                    for (loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
-                        System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
+        if (shape == 't') {
+
+            for (int loopDir = 0; loopDir < 2; loopDir++) {
+                if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
+                    for (loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
+                        for (loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
+                            System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
+                        }
+                        System.out.println();
                     }
-                    System.out.println();
+                } else {
+                    for (loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
+                        for (loop_num = 1; loop_num <= loop_iter; loop_num++) {
+                            System.out.print(" " + loop_num);
+                        }
+                        System.out.println();
+                    }
                 }
-            } else {
-                for (loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
-                    for (loop_num = 1; loop_num <= loop_iter; loop_num++) {
-                        System.out.print(" " + loop_num);
+            }
+
+        } else if (shape == 'd'){
+            for (int loopDir = 0; loopDir < 2; loopDir++) {
+                if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
+                    for (loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
+                        for (loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
+                            System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
+                        }
+                        System.out.println();
                     }
-                    System.out.println();
+                } else {
+                    for (loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
+                        for (loop_num = 1; loop_num <= loop_iter; loop_num++) {
+                            System.out.print(" " + loop_num);
+                        }
+                        System.out.println();
+                    }
                 }
             }
         }
-
     }
 }
