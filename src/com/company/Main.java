beed55192e022d0006dff7be6500690ee5f0004b
Kiran <kiran@inteleyes.com>
2021-09-23 10:58:58 +0100

    Split shape generation in to methods.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,6 +18,47 @@ public class Main {
         return 'A'; // will never reach this
     }
 
+    public static void generateTriangle(int maxNum) {
+        for (int loopDir = 0; loopDir < 2; loopDir++) {
+            if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
+                for (int loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
+                    for (int loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
+                        System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
+                    }
+                    System.out.println();
+                }
+            } else {
+                for (int loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
+                    for (int loop_num = 1; loop_num <= loop_iter; loop_num++) {
+                        System.out.print(" " + loop_num);
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+
+    public static void generateDiamond(int maxNum) {
+        for (int loopDir = 0; loopDir < 2; loopDir++) {
+            if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
+                for (int loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
+                    for (int loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
+                        System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
+                    }
+                    System.out.println();
+                }
+            } else {
+                for (int loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
+                    for (int loop_num = 1; loop_num <= loop_iter; loop_num++) {
+                        System.out.print(" " + loop_num);
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new Scanner(System.in);
@@ -27,47 +68,14 @@ public class Main {
         System.out.println("Type the maximum number: \n");
         int maxNum = scanner.nextInt();
 
-        int loop_iter;
-        int loop_num;
+
 
         if (shape == 't') {
 
-            for (int loopDir = 0; loopDir < 2; loopDir++) {
-                if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
-                    for (loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
-                        for (loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
-                            System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
-                        }
-                        System.out.println();
-                    }
-                } else {
-                    for (loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
-                        for (loop_num = 1; loop_num <= loop_iter; loop_num++) {
-                            System.out.print(" " + loop_num);
-                        }
-                        System.out.println();
-                    }
-                }
-            }
+            generateTriangle(maxNum);
 
-        } else if (shape == 'd'){
-            for (int loopDir = 0; loopDir < 2; loopDir++) {
-                if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
-                    for (loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
-                        for (loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
-                            System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
-                        }
-                        System.out.println();
-                    }
-                } else {
-                    for (loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
-                        for (loop_num = 1; loop_num <= loop_iter; loop_num++) {
-                            System.out.print(" " + loop_num);
-                        }
-                        System.out.println();
-                    }
-                }
-            }
+        } else if (shape == 'd') {
+            generateDiamond(maxNum);
         }
     }
 }
