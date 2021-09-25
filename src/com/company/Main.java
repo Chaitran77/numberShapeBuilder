b61acf698206b14e1585c5dddbf62216c5f39712
Kiran <kiran@inteleyes.com>
2021-09-25 20:00:32 +0100

    Can now generate an undistorted number-diamond going up to numbers with multiple digits using a dynamic .repeat() on the spacer string.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void generateTriangle(int maxNum) {
         for (int loopDir = 0; loopDir < 2; loopDir++) {
-            if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
+            if ((loopDir % 2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
                 for (int loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
                     for (int loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
                         System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
@@ -40,34 +40,62 @@ public class Main {
 
 
     public static void generateDiamond(int maxNum) {
+
+        int max_num_length = String.valueOf(maxNum).length();
+
         for (int loopDir = 0; loopDir < 2; loopDir++) {
-            if ((loopDir%2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
-                for (int loop_iter = 1; loop_iter <= maxNum; loop_iter++) {
-                    for (int loop_num = 1; loop_num < loop_iter; loop_num++) { // made condition only less than to eliminate one of the max's
-                        System.out.print(" " + loop_num); // print out the current number (with a space before it as a separator)
+            if ((loopDir % 2) == 0) { // check the modulus of loopDir/2 to determine forward or reverse direction
+                for (int loop_iter = 0; loop_iter <= maxNum; loop_iter++) {
+                    int curr_num_length = String.valueOf(loop_iter).length();
+
+                    for (int line_index = -maxNum/2; line_index <= maxNum*2; line_index++) {
+
+                        if ((line_index <= (maxNum / 2) + loop_iter) && (line_index >= (maxNum / 2) - loop_iter)) {
+                            System.out.print(" ".repeat(max_num_length-curr_num_length) + loop_iter);
+
+//                            System.out.println(loop_num >= range);
+//                            System.out.println(loop_num <= maxNum-range);
+//                            System.out.println((loop_num >= range) && (loop_num <= maxNum-range));
+                        } else {
+                            System.out.print(" ".repeat(max_num_length));
+                        }
                     }
-                    System.out.println();
+
+                        System.out.println();
+
                 }
             } else {
                 for (int loop_iter = maxNum; loop_iter >= 0; loop_iter--) {
-                    for (int loop_num = 1; loop_num <= loop_iter; loop_num++) {
-                        System.out.print(" " + loop_num);
+                    int curr_num_length = String.valueOf(loop_iter).length();
+
+                    for (int line_index = -maxNum/2; line_index <= maxNum*2; line_index++) {
+
+                        if ((line_index <= (maxNum / 2) + loop_iter) && (line_index >= (maxNum / 2) - loop_iter)) {
+                            System.out.print(" ".repeat(max_num_length-curr_num_length) + loop_iter);
+
+//                            System.out.println(loop_num >= range);
+//                            System.out.println(loop_num <= maxNum-range);
+//                            System.out.println((loop_num >= range) && (loop_num <= maxNum-range));
+                        } else {
+                            System.out.print(" ".repeat(max_num_length));
+                        }
                     }
+
                     System.out.println();
+
                 }
             }
         }
     }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner scanner = new Scanner(System.in);
 
         char shape = getShape(scanner);
 
         System.out.println("Type the maximum number: \n");
         int maxNum = scanner.nextInt();
-
 
 
         if (shape == 't') {
